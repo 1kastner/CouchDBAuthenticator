38599608f98175463919c0dbd932135a948034e7
Marvin Kastner <marvin.kastner@tuhh.de>
2022-01-25 12:24:48 +0100

    Improve prepare_publish.bat

--- prepare_publish.bat
+++ prepare_publish.bat
@@ -1,7 +1,26 @@
-RMDIR /s build dist jupyterhub_couchdb_authenticator.egg-info
+@ECHO OFF
 
-CALL python setup.py sdist bdist_wheel
+ECHO.
+ECHO Delete previously published versions
+RMDIR /s build dist *.egg-info
+ECHO Deleting finished
 
+ECHO.
+ECHO Create files that are meant to be distributed
+CALL python setup.py sdist bdist_wheel || PAUSE
+ECHO.
+ECHO Creation finished
+
+ECHO Checking files that are meant to be distributed
 CALL twine check .\dist\*
+ECHO.
+ECHO Please check whether the twine check passed in all instances.
+PAUSE
 
-ECHO With 'twine upload dist/*', you upload the new version now.
+ECHO.
+ECHO Now, you can test the new version by uploading it with the command 'twine upload -r testpypi dist/*'
+PAUSE
+
+ECHO.
+ECHO Afterwards, you can upload the new version with 'twine upload dist/*'.
+PAUSE
