a3d89e16732859bf1cc1933d31fe8f06ddec9941
Marvin Kastner <marvin.kastner@tuhh.de>
2022-01-25 12:11:03 +0100

    Use debug for logging and improve exceptions

--- couchdbauthenticator/authenticator.py
+++ couchdbauthenticator/authenticator.py
@@ -2,10 +2,10 @@
 The authenticator code that is used on the JupyterHub for authenticating people by the entries in a database.
 """
 
-from tornado import gen
-from jupyterhub.auth import Authenticator
-from traitlets import Unicode, Bool
 import requests
+from jupyterhub.auth import Authenticator
+from tornado import gen
+from traitlets import Unicode, Bool
 
 
 # noinspection PyUnresolvedReferences
@@ -47,8 +47,7 @@ class CouchDBAuthenticator(Authenticator):
                 "username": username,
                 "active": True
             },
-            "fields": ["username", "password"],
-            "limit": 1
+            "fields": ["username", "password"]
         }
         response = requests.post(
             search_url,
@@ -56,20 +55,29 @@ class CouchDBAuthenticator(Authenticator):
             auth=requests.auth.HTTPBasicAuth(self.couchdb_username, self.couchdb_password),
             verify=self.ssl_verification
         )
+
+        try:
+            response.raise_for_status()  # raise exception for HTTP error codes
+        except requests.HTTPError as exception:
+            self.log.error(exception, exc_info=True)
+            return
+
         parsed_response = response.json()
-
-        response.raise_for_status()
-
         if not hasattr(parsed_response, "keys") or "docs" not in parsed_response.keys():
-            raise Exception(f"Malformed response of CouchDB search query: '{response.text}'")
+            self.log.error(f"Malformed response of CouchDB search query: '{response.text}' "
+                           f"with status code '{response.status_code}'")
+            return
 
         if len(parsed_response["docs"]) == 0:
-            self.log.info(f"Provided user name '{username}' not existent")
+            self.log.debug(f"Provided user name '{username}' not existent")
+            return
+        if len(parsed_response["docs"]) > 1:
+            self.log.warning(f"For the username '{username}' duplicate entries exist in the database, please tidy up!")
             return
         retrieved_user = parsed_response["docs"][0]
         actual_password = retrieved_user["password"]
         if actual_password != provided_password:
-            self.log.info(f"Provided password of '{username}' did not match")
+            self.log.debug(f"Provided password of '{username}' did not match")
             return
 
         self.log.debug(f"User '{username}' logged in")
--- minimal-jupyterhub-example/jupyterhub/jupyterhub_config.py
+++ minimal-jupyterhub-example/jupyterhub/jupyterhub_config.py
@@ -1,10 +1,14 @@
 import os
+import logging
 
 c.JupyterHub.ssl_key = '/srv/jupyterhub/cert/key.pem'
 c.JupyterHub.ssl_cert = '/srv/jupyterhub/cert/cert.pem'
 
 c.JupyterHub.authenticator_class = 'couchdbauthenticator.CouchDBAuthenticator'
+
 c.CouchDBAuthenticator.couchdb_url = "couchdb:6984"
 c.CouchDBAuthenticator.ssl_verification = False
 c.CouchDBAuthenticator.couchdb_username = os.environ["COUCHDB_USER"]
 c.CouchDBAuthenticator.couchdb_password = os.environ["COUCHDB_PASSWORD"]
+
+c.JupyterHub.log_level = logging.DEBUG
